9c755f639fff28385998eafb861a4a165119299e
visrut <visrutnavadiya111@gmail.com>
2025-03-30 22:39:39 +0530

    add posthog

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,21 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
+import { PostHogProvider} from 'posthog-js/react'
+
+const options = {
+  api_host: import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PostHogProvider 
+      apiKey={import.meta.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PostHogProvider>
   </StrictMode>
 );
